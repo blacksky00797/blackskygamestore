53950007f78ff8b20248a184dafea11aaf7c049c
blacksky00797 <58395584+blacksky00797@users.noreply.github.com>
2021-09-13 13:29:41 +0630

    Add files via upload

---
+++ js/gamedata.js
@@ -0,0 +1,157 @@
+gameData = [
+    {
+        title: "Outriders",
+        genre: "Third person shooter",
+        publisher: "Square Enix",
+        platform: "PC",
+        size: "43.0 GB",
+        logoLink: "https://i.ibb.co/Lr7PcPn/Outriders-logo.jpg",
+        gameplayLinks: [
+            "https://i.ibb.co/6DZpXng/Outriders-gameplay-3.jpg",
+            "https://i.ibb.co/tsNbwYM/Outriders-gameplay-2.jpg",
+            "https://i.ibb.co/vXZ0b8k/Outriders-gameplay-1.jpg",
+            "https://i.ibb.co/3SQJF7C/Outriders-gameplay-4.png"
+        ],
+        systemRequirements: {
+            minimum: {
+                os: "Windows 10/8/7 ( 64 bit )",
+                cpu: "Intel Core i5-3470/ AMD FX-8350",
+                gpu: "Nvidia GeForce GTX 750ti/ AMD Radeon R9 270x",
+                ram: "8 GB",
+                directX: "Version 11",
+            },
+            recommend: {
+                os: "Windows 10/8/7 ( 64 bit )",
+                cpu: "Intel Core i7-7700/ AMD Ryzen 5 1600",
+                gpu: "Nvidia Geforce GTX 1060 6GB/ AMD Radeon RX 480 8GB",
+                ram: "16 GB",
+                directX: "Version 12",
+            }
+        }
+    } , 
+
+    {
+        title: "A Plague Tale: Innocence",
+        genre: "Survival game",
+        publisher: "Focus Entertainment",
+        platform: "PC",
+        size: "10.1 GB",
+        logoLink: "https://i.ibb.co/FDrsnHX/a-plague-tale-innocence-logo.png",
+        gameplayLinks: [
+            "https://i.ibb.co/FDrsnHX/a-plague-tale-innocence-logo.png",
+            "https://i.ibb.co/FnR93R1/a-plague-tale-innocence-gameplay-1.jpg",
+            "https://i.ibb.co/pxvXZT4/a-plague-tale-innocence-gameplay-2.jpg",
+            "https://i.ibb.co/rFHLDfS/a-plague-tale-innocence-gameplay-3.jpg"
+        ],
+        systemRequirements: {
+            minimum: {
+                os: "Windows 7/8/10 (64 bits)",
+                cpu: "Intel Core i3-2120 (3.3 GHz)/AMD FX-4100 X4 (3.6 GHz)",
+                gpu: "2 GB, GeForce GTX 660/Radeon HD 7870",
+                ram: "8 GB",
+                directX: "Version 11",
+            },
+            recommend: {
+                os: "Windows 10/8/7 ( 64 bit )",
+                cpu: "Intel Core i5-4690 (3.5 GHz)/AMD FX-8300 (3.3 GHz)",
+                gpu: "4 GB, GeForce GTX 970/Radeon RX 480",
+                ram: "16 GB",
+                directX: "Version 12",
+            }
+        }
+    },
+
+    {
+        title: "It Takes Two",
+        genre: "Pazzle,Action-Adventure",
+        publisher: "Electronic Arts",
+        platform: "PC",
+        size: "27.3 GB",
+        logoLink: "https://i.ibb.co/GWs5KFT/It-takes-two-logo.jpg",
+        gameplayLinks: [
+            "https://i.ibb.co/RScBFJc/It-takes-two-gameplay-4.jpg",
+            "https://i.ibb.co/Zg3kZRh/It-takes-two-gameplay-2.jpg",
+            "https://i.ibb.co/c6mz6ZB/It-takes-two-gameplay-3.jpg",
+            "https://i.ibb.co/QMsJ6dz/It-takes-two-gameplay-1.jpg"
+        ],
+        systemRequirements: {
+            minimum: {
+                os: "Windows 8.1 64-bit or Windows 10 64-bit",
+                cpu: " Intel Core i3-2100T or AMD FX 6100",
+                gpu: "Nvidia GeForce GTX 660 or AMD R7 260x",
+                ram: "8 GB",
+                directX: "Version 11",
+            },
+            recommend: {
+                os: "Windows 8.1 64-bit or Windows 10 64-bit",
+                cpu: "Intel Core i5 3570K or AMD Ryzen 3 1300x",
+                gpu: "Nvidia GeForce GTX 980 or AMD R9 290X",
+                ram: "16 GB",
+                directX: "Version 11",
+            }
+        }
+    },
+
+    {
+        title: "Biomutant",
+        genre: "Action role-playing,Nonlinear",
+        publisher: "THQ Nordic",
+        platform: "PC",
+        size: "14.5 GB",
+        logoLink: "https://i.ibb.co/C1wgPnh/Biomutant-logo.jpg",
+        gameplayLinks: [
+            "https://i.ibb.co/Y2Kwq4T/Biomutant-gameplay-4.jpg",
+            "https://i.ibb.co/qpCmxQ8/Biomutant-gameplay-3.jpg",
+            "https://i.ibb.co/VJFgL4S/Biomutant-gameplay-2.jpg",
+            "https://i.ibb.co/f2H5c99/Biomutant-gameplay-1.jpg"
+        ],
+        systemRequirements: {
+            minimum: {
+                os: "Windows 7/8.1/10 (64 bit)",
+                cpu: "AMD FX-8350 or Intel Core i5-4690K or newer running at 3.5 GHz or higher",
+                gpu: "4 GB Direct3D 11 capable video card - GeForce GTX 960 or Radeon R9 380",
+                ram: "8 GB",
+                directX: "Version 11",
+            },
+            recommend: {
+                os: "Windows 10 (64bit)",
+                cpu: "AMD Ryzen 5 1600 or Intel Core i7-6700K or newer running at 3.2 GHz or higher",
+                gpu: "6 GB Direct3D 11 capable video card - GeForce GTX 1660Ti or Radeon RX 590",
+                ram: "16 GB",
+                directX: "Version 11",
+            }
+        }
+    },
+    
+    {
+        title: "GhostRunner",
+        genre: "Platform,Fighting,Adventure",
+        publisher: "All in! Games SA, 505 Games",
+        platform: "PC",
+        size: "12.0 GB",
+        logoLink: "https://i.ibb.co/BBC8HRX/Ghost-Runner-logo.jpg",
+        gameplayLinks: [
+            "https://i.ibb.co/Zh5rvWW/Ghostrunner-gameplay-4.jpg",
+            "https://i.ibb.co/jzWLz4h/Ghost-Runner-gameplay-3.jpg",
+            "https://i.ibb.co/YPHsbKX/Ghost-Runner-gameplay-2.jpg",
+            "https://i.ibb.co/Jrd3jfx/Ghost-Runner-gameplay-1.jpg"
+        ],
+        systemRequirements: {
+            minimum: {
+                os: "Windows 7, 8.1, 10 x64",
+                cpu: "Intel Core i5-2500K (4 * 3300) AMD Phenom II X4 965 (4 * 3400) or equivalent",
+                gpu: "GeForce GTX 1050 (2048 MB) / Radeon RX 550 (4096 MB)",
+                ram: "8 GB",
+                directX: "Version 11",
+            },
+            recommend: {
+                os: "Windows 7, 8.1, 10 x64",
+                cpu: "Intel Core i7-6700K (4 * 4000) or AMD Ryzen 5 1500X (4 * 3500) or equivalent",
+                gpu: "GeForce GTX 970 (4096 MB) or Radeon RX 5700 (8192 MB)",
+                ram: "8 GB",
+                directX: "Version 12",
+            }
+        }
+    },
+
+]
